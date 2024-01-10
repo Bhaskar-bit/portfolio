88db609223e8ea8359ba9c1f5167e564a00735de
Bhaskar-bit <bhaskarsrj@gmail.com>
2024-01-10 14:56:36 +0530

    skill updated

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -10,7 +10,7 @@ import { locale as french } from '../shared/i18n/fr';
 })
 export class SkillsComponent implements OnInit {
   skills:any={
-    technologies: [{ name: "JavaScript, Typescript, Angular", percent: 90, remark: 'excellent' },{ name: "HTML 5, CSS 3, Bootstrap", percent: 90, remark: 'excellent' }, { name: "D3js, Nodejs", percent: 70, remark: 'good' }, { name: "SQL", percent: 70, remark: 'good' }, { name: ".Net, C#", percent: 60, remark: 'good' }],
+    technologies: [{ name: "JavaScript, Typescript, Angular", percent: 90, remark: 'excellent' },{ name: "HTML 5, CSS 3, Bootstrap", percent: 90, remark: 'excellent' }, { name: "D3js, Nodejs", percent: 70, remark: 'good' }, { name: "Accessiblity", percent: 90, remark: 'excellent' }, { name: ".Net, C#", percent: 60, remark: 'good' }],
             tools: [{ name: "Git", percent: 90, remark: 'excellent' }, { name: "Office", percent: 90, remark: 'excellent' }, { name: "Linux, Windows", percent: 70, remark: 'very-good' }, { name: "MySql, PgSql", percent: 90, remark: 'excellent' }, { name: "Cloud Platforms (AWS,GCP)", percent: 50, remark: 'average' }],
             methodologies: [{ name: "Scrum", percent: 70, remark: 'very-good' },{ name: "Design Thinking", percent: 70, remark: 'good' }, { name: "TDD", percent: 90, remark: 'excellent' }, { name: "DevOps", percent: 50, remark: 'average' }]
         };
